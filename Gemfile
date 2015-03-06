86605acc2eb1d0e2a4c019db55a84a8d9f62b476
C Haden <chaden@spoonflower.com>
2015-03-06 18:47:43 -0500

    many experiemnts. maybe pictaculous suppost is on the way

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
+ruby '2.2.0'
 
+gem 'rubyctaculous', '~> 0.0.1'
+gem 'httparty', '~> 0.9.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
@@ -42,4 +45,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,30 @@
 class ImagesController < ApplicationController
   def show
+    @results = Image.new.results
+
+    # EXPERIMENT: can open file as binary to feed to pictaculous?
+    image = "./app/assets/images/duckling.jpg"
+    # s = File.open("./app/assets/images/dont_starve.jpg", 'r') { |io| io.read }
+    s = File.open(image, "rb") { |f| f.read }
+    @binary = s
+    @encoding = s.encoding
+
+    body = {image: s}
+    @response = JSON.parse(HTTParty.post("http://pictaculous.com/api/1.0", :body => body))
+    # END EXPERIMENT
+
+
+
+
+    # EXPERIMENT: example code for pictaculous gem
+    # pictaculous = Pictaculous::Client.new
+    #
+    # @colors = pictaculous.color_palette_for_image('./app/assets/images/duckling.jpg')
+
+    # puts colors.inspect
+    # => ['FFFFD3', '372C78', '9D7891', '654A86'] # Hex format
+    # END EXPERIMENT
+
+
   end
 end
