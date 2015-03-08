9995183104f024636f72f68f2aa109e71d4940cd
Maria <mliberov@gmail.com>
2015-03-08 17:26:25 -0400

    Add model test

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -6,4 +6,6 @@ class ImagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  
+
 end
---
+++ test/models/image_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ImageTest
+
+  test "images can exist" do
+    assert Image.new
+  end
+
+end
