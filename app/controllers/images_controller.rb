25e3e7fb25fa9f573f29d3ec1e697c3246b9112d
C Haden <chaden@spoonflower.com>
2015-03-08 16:35:29 -0400

    remove comment cruft #shipit

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -22,46 +22,5 @@ class ImagesController < ApplicationController
     end
 
     render json: {bunny_images: bunny_images}
-
-
-    #PICTACULOUS RESOURCES FOR LATER
-    # http://www.pictaculous.com/api/
-    # https://github.com/tatemae-consultancy/pictaculous
-    # https://github.com/basgys/rubyctaculous/blob/master/lib/pictaculous/client.rb
-    #END RESOURCES
-
-    # EXPERIMENT: can open file as binary to feed to pictaculous?
-    # image = "./app/assets/images/duckling.jpg"
-    # s = File.open(image, "rb") { |f| f.read }
-    # @binary = s
-    # @encoding = s.encoding
-    #
-    # body = {image: s}
-    # @response = JSON.parse(HTTParty.post("http://pictaculous.com/api/1.0", :body => body))
-    # END EXPERIMENT
-
-
-    #EXPERIMENT: From Michael, render straight json, no need for a view
-    # r = {this: "is a test", results: Image.new.results }
-    #
-    # render json: @getty_results
-    #END EXPERIMENT
-
-
-    # render json: @flickr_results
-
-
-
-
-    # EXPERIMENT: example code for pictaculous gem
-    # pictaculous = Pictaculous::Client.new
-    #
-    # @colors = pictaculous.color_palette_for_image('./app/assets/images/duckling.jpg')
-
-    # puts colors.inspect
-    # => ['FFFFD3', '372C78', '9D7891', '654A86'] # Hex format
-    # END EXPERIMENT
-
-
   end
 end
