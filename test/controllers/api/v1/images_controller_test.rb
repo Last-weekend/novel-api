b2c09868cd4afa82ec932dad90517de12381985c
C Haden <chaden@spoonflower.com>
2015-03-08 17:46:30 -0400

    remove unused code

--- test/controllers/api/v1/images_controller_test.rb
+++ test/controllers/api/v1/images_controller_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :index
     assert_response :success
   end
 
-  
+
 
 end
--- test/models/image_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ImageTest
-
-  test "images can exist" do
-    assert Image.new
-  end
-
-end
